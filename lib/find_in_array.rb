fe5cb69af0741d3e52cb034fb13eb94339e58836
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-29 23:31:44 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
   for i in array do
     if array[i] == value_to_find do
-      puts
+      puts i 
+    end
+  end
 end
+
+find_element_index([1,2,3,4], 3)
